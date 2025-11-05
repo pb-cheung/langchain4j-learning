0d5515677a5a4f160cf0e7a2924d55f71d670412
pb-cheung <pbcheung@vip.qq.com>
2025-11-05 16:12:13 +0800

    add http proxy

--- src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
+++ src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
@@ -5,6 +5,7 @@ import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -12,27 +13,51 @@ public class LLMTest {
     @Autowired
     private OpenAiChatModel openAiChatModel;
 
+    @Autowired
+    private OllamaChatModel ollamaChatModel;
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Value("${PROXY_HOST}")
+    private String proxyHost;
+
+    @Value("${PROXY_PORT}")
+    private String proxyPort;
+
+    @Value("${ENABLE_PROXY:false}")
+    private boolean enableProxy;
+
+    // 设置代理
+    private void setProxyIfEnabled() {
+        if (enableProxy) {
+            System.setProperty("https.proxyHost", proxyHost);
+            System.setProperty("https.proxyPort", proxyPort);
+            System.out.println("代理已启用，代理地址：https://" + proxyHost + ":" + proxyPort);
+        } else {
+            System.out.println("代理未启用");
+        }
+    }
+
+
     @Test
     public void testGPTDemo() {
         OpenAiChatModel model = OpenAiChatModel.builder()
-                .baseUrl("http://langchain4j.dev/demo/openai/v1")
-                .apiKey("demo")
-                .modelName("gpt-4o-mini")
-                .build();
+            .baseUrl("http://langchain4j.dev/demo/openai/v1")
+            .apiKey("demo")
+            .modelName("gpt-4o-mini")
+            .build();
 
         String answer = model.chat("你好，你是谁？ ");
         System.out.println(answer);
-
     }
 
     @Test
     public void testOpenAIDemo() {
-        String answer = openAiChatModel.chat("怀孕26周左右，孕妇肚皮不定时的会有紧绷状态和硬块，可能是什么原因？");
-        System.out.println(answer );
+        setProxyIfEnabled();
+        String answer = openAiChatModel.chat("你是谁？");
+        System.out.println(answer);
     }
-
-    @Autowired
-    private OllamaChatModel ollamaChatModel;
 
     @Test
     public void testOllamaChatModel() {
@@ -40,12 +65,9 @@ public class LLMTest {
         System.out.println(answer);
     }
 
-
-    @Autowired
-    private QwenChatModel qwenChatModel;
-
     @Test
     public void testQwenChatModel() {
+        setProxyIfEnabled();
         String answer = qwenChatModel.chat("你是谁？");
         System.out.println(answer);
     }
