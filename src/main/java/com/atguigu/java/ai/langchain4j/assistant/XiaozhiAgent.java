e5941c0cc5fba5bbb98b6b50a030805b09bf4ed3
pb-cheung <pbcheung@vip.qq.com>
2025-11-17 22:41:08 +0800

    项目实战

---
+++ src/main/java/com/atguigu/java/ai/langchain4j/assistant/XiaozhiAgent.java
@@ -0,0 +1,21 @@
+package com.atguigu.java.ai.langchain4j.assistant;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+@AiService(
+        wiringMode = EXPLICIT,
+        chatModel = "qwenChatModel",
+        chatMemoryProvider = "chatMemoryProviderXiaozhi"
+)
+public interface XiaozhiAgent {
+
+    @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
+    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+
+
+}
